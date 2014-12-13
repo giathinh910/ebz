ab5af7f3ba1463551ac392844db1d7498c5b0e1f
Thinh Bui <thinhbg@haintheme.com>
2014-12-14 00:18:55 +0700

    Restyle footer

--- application/views/front/layout/foot.php
+++ application/views/front/layout/foot.php
@@ -1,28 +1,7 @@
 <footer class="footer">
 	<div class="container">
-		<div class="clearfix">
-			<div class="footer-logo">
-				<a href="#"><i class="fa fa-map-marker gt-logo"></i> EBZ</a>
-			</div>
-			<dl class="footer-nav">
-				<dt class="nav-title">ĐỊA ĐIỂM</dt>
-				
-			</dl>
-			<dl class="footer-nav">
-				<dt class="nav-title">ABOUT</dt>
-				<dd class="nav-item"><a href="#">The Company</a></dd>
-				<dd class="nav-item"><a href="#">History</a></dd>
-				<dd class="nav-item"><a href="#">Vision</a></dd>
-			</dl>
-			<dl class="footer-nav"><dt class="nav-title">GALLERY</dt><dd class="nav-item"><a href="#">Flickr</a></dd>
-				<dd class="nav-item"><a href="#">Picasa</a></dd>
-				<dd class="nav-item"><a href="#">iStockPhoto</a></dd>
-				<dd class="nav-item"><a href="#">PhotoDune</a></dd>
-			</dl>
-			<dl class="footer-nav"><dt class="nav-title">CONTACT</dt><dd class="nav-item"><a href="#">Basic Info</a></dd>
-				<dd class="nav-item"><a href="#">Map</a></dd>
-				<dd class="nav-item"><a href="#">Conctact Form</a></dd>
-			</dl>
+		<div class="text-center" style="font-size: 40px;">
+			<a href="#"><i class="fa fa-map-marker gt-logo" style="font-size: 50px;"></i> EBZ</a>
 		</div>
 		<div class="footer-copyright text-center">Copyright © 2014 EBZ.All rights reserved.</div>
 	</div>
